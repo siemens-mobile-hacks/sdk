316e9425777ca098fac3dec88f1efef971be4a24
Philip Minakov <sir.cadogan@protonmail.com>
2024-03-17 19:21:38 +0400

    Convert mplayer.h to utf-8 encoding

--- include/mplayer.h
+++ include/mplayer.h
@@ -1,28 +1,28 @@
-// Функции управления медиаплеером
+// Р¤СѓРЅРєС†РёРё СѓРїСЂР°РІР»РµРЅРёСЏ РјРµРґРёР°РїР»РµРµСЂРѕРј
 // (c) Kibab
-// Версия 0.4
+// Р’РµСЂСЃРёСЏ 0.4
 // (r) All S-C Community
 
-/* Лог изменений 
-0.4 - inline, добавлен комплект функций //BoBa
-0.3 - добавлены функции выключения плеера, выключения звука
-0.2 - добавлена функция изменения громкости
-0.1 - первая публичная версия
+/* Р›РѕРі РёР·РјРµРЅРµРЅРёР№ 
+0.4 - inline, РґРѕР±Р°РІР»РµРЅ РєРѕРјРїР»РµРєС‚ С„СѓРЅРєС†РёР№ //BoBa
+0.3 - РґРѕР±Р°РІР»РµРЅС‹ С„СѓРЅРєС†РёРё РІС‹РєР»СЋС‡РµРЅРёСЏ РїР»РµРµСЂР°, РІС‹РєР»СЋС‡РµРЅРёСЏ Р·РІСѓРєР°
+0.2 - РґРѕР±Р°РІР»РµРЅР° С„СѓРЅРєС†РёСЏ РёР·РјРµРЅРµРЅРёСЏ РіСЂРѕРјРєРѕСЃС‚Рё
+0.1 - РїРµСЂРІР°СЏ РїСѓР±Р»РёС‡РЅР°СЏ РІРµСЂСЃРёСЏ
 */
 
 #ifndef _MPLAYER_H_
   #define _MPLAYER_H_
 
-// Команды плеера
+// РљРѕРјР°РЅРґС‹ РїР»РµРµСЂР°
 #define PLAYER_PREV	0x01
 #define PLAYER_NEXT	0x02
 #define PLAYER_RESTART	0x03
-//0x04 а хз... часики рисует... БД??
-//0x05 хрень какая-то с кнопками...
-//0x06 роняет
-//0x07 рестарт
-//0x08 рестарт
-//0x09 рестарт
+//0x04 Р° С…Р·... С‡Р°СЃРёРєРё СЂРёСЃСѓРµС‚... Р‘Р”??
+//0x05 С…СЂРµРЅСЊ РєР°РєР°СЏ-С‚Рѕ СЃ РєРЅРѕРїРєР°РјРё...
+//0x06 СЂРѕРЅСЏРµС‚
+//0x07 СЂРµСЃС‚Р°СЂС‚
+//0x08 СЂРµСЃС‚Р°СЂС‚
+//0x09 СЂРµСЃС‚Р°СЂС‚
 #define PLAYER_MUTE	0x0A
 //0x0b kill?
 #define PLAYER_KILL	0x0C
@@ -40,13 +40,13 @@
 #define PLAYER_REPEAT1	0x18
 //0x19 error?
 //0x20
-//0x21 роняет
+//0x21 СЂРѕРЅСЏРµС‚
 
-void Send_MPlayer_Command(unsigned short cmd, short substract){		// Отправить команду плееру
+void Send_MPlayer_Command(unsigned short cmd, short substract){		// РћС‚РїСЂР°РІРёС‚СЊ РєРѕРјР°РЅРґСѓ РїР»РµРµСЂСѓ
 	GBS_SendMessage(0x4209, 0x8053, 0, *((unsigned int*)RamMPlayer_CtrlCode())-substract,cmd);
 }
 
-void MPlayer_VolChange(short dir){					// Громкость вверх/вниз (dir=1 / dir=-1)
+void MPlayer_VolChange(short dir){					// Р“СЂРѕРјРєРѕСЃС‚СЊ РІРІРµСЂС…/РІРЅРёР· (dir=1 / dir=-1)
 	int cmd;
 	if(dir==1){
     		cmd = PLAYER_VOL_UP;
@@ -57,14 +57,14 @@ void MPlayer_VolChange(short dir){					// Громкость вверх/вниз (dir=1 / dir=-1)
 	Send_MPlayer_Command(cmd,0);
 }
 
-inline void MPlayer_Stop()	{Send_MPlayer_Command(PLAYER_STOP,0);}	// Остановить игру
-inline void MPlayer_Start()	{Send_MPlayer_Command(PLAYER_PLAY,0);}	// Запуск проигрывания
-inline void MPlayer_Next()	{Send_MPlayer_Command(PLAYER_NEXT,0);}	// Переключиться на следующую песню
-inline void MPlayer_Prev()	{Send_MPlayer_Command(PLAYER_PREV,0);}	// Переключиться на предыдущую песню
-inline void MPlayer_Toggle()	{Send_MPlayer_Command(PLAYER_TOGGLE,0);}// Пауза/воспроизведение
-inline void MPlayer_Shutdown()	{Send_MPlayer_Command(PLAYER_KILL,0);}	// Завершить работу плеера
-inline void MPlayer_Mute()	{Send_MPlayer_Command(PLAYER_MUTE,0);}	// Выключить звук. При повторном вызове звук включается
-inline void MPlayer_Pause()	{Send_MPlayer_Command(PLAYER_PAUSE,0);}	// Пауза
+inline void MPlayer_Stop()	{Send_MPlayer_Command(PLAYER_STOP,0);}	// РћСЃС‚Р°РЅРѕРІРёС‚СЊ РёРіСЂСѓ
+inline void MPlayer_Start()	{Send_MPlayer_Command(PLAYER_PLAY,0);}	// Р—Р°РїСѓСЃРє РїСЂРѕРёРіСЂС‹РІР°РЅРёСЏ
+inline void MPlayer_Next()	{Send_MPlayer_Command(PLAYER_NEXT,0);}	// РџРµСЂРµРєР»СЋС‡РёС‚СЊСЃСЏ РЅР° СЃР»РµРґСѓСЋС‰СѓСЋ РїРµСЃРЅСЋ
+inline void MPlayer_Prev()	{Send_MPlayer_Command(PLAYER_PREV,0);}	// РџРµСЂРµРєР»СЋС‡РёС‚СЊСЃСЏ РЅР° РїСЂРµРґС‹РґСѓС‰СѓСЋ РїРµСЃРЅСЋ
+inline void MPlayer_Toggle()	{Send_MPlayer_Command(PLAYER_TOGGLE,0);}// РџР°СѓР·Р°/РІРѕСЃРїСЂРѕРёР·РІРµРґРµРЅРёРµ
+inline void MPlayer_Shutdown()	{Send_MPlayer_Command(PLAYER_KILL,0);}	// Р—Р°РІРµСЂС€РёС‚СЊ СЂР°Р±РѕС‚Сѓ РїР»РµРµСЂР°
+inline void MPlayer_Mute()	{Send_MPlayer_Command(PLAYER_MUTE,0);}	// Р’С‹РєР»СЋС‡РёС‚СЊ Р·РІСѓРє. РџСЂРё РїРѕРІС‚РѕСЂРЅРѕРј РІС‹Р·РѕРІРµ Р·РІСѓРє РІРєР»СЋС‡Р°РµС‚СЃСЏ
+inline void MPlayer_Pause()	{Send_MPlayer_Command(PLAYER_PAUSE,0);}	// РџР°СѓР·Р°
 
 
 #endif
